4c6133a831e119728ebb0619a23d25bc8662103e
oui <mohamed_elhani@outlook.fr>
2025-10-26 21:29:06 +0100

    Actually pushing an unfinished reader

--- src/main/java/ringed_strawberry/github/io/tailored_tools/TailoredTools.java
+++ src/main/java/ringed_strawberry/github/io/tailored_tools/TailoredTools.java
@@ -1,6 +1,9 @@
 package ringed_strawberry.github.io.tailored_tools;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
+import ringed_strawberry.github.io.tailored_tools.dataReading.TailoredToolsReader;
 
 import static ringed_strawberry.github.io.tailored_tools.block.ModBlocks.registerModBlocks;
 import static ringed_strawberry.github.io.tailored_tools.block.entity.ModBlockEntities.registerModBlockEntities;
@@ -11,5 +14,7 @@ public class TailoredTools implements ModInitializer {
     public void onInitialize() {
         registerModBlocks();
         registerModBlockEntities();
+        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new TailoredToolsReader());
+
     }
 }
--- src/main/java/ringed_strawberry/github/io/tailored_tools/dataReading/TailoredToolsReader.java
+++ src/main/java/ringed_strawberry/github/io/tailored_tools/dataReading/TailoredToolsReader.java
@@ -1,4 +1,25 @@
 package ringed_strawberry.github.io.tailored_tools.dataReading;
 
-public class TailoredToolsReader {
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+
+import java.io.InputStream;
+
+public class TailoredToolsReader implements SimpleSynchronousResourceReloadListener {
+    @Override
+    public Identifier getFabricId(){
+        return Identifier.of("Materials","tailored_tools_materials");
+    }
+
+    @Override
+    public void reload(ResourceManager manager){
+        for (Identifier id: manager.findResources("tailored_tools_materials", path -> path.toString().endsWith(".json")).keySet()){
+            try {
+                InputStream stream = manager.getResource(id).get().getInputStream();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
